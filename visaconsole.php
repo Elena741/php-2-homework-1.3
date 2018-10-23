1a55ef3b6245b3b30cac7c24dc099dd5d225c271
Elena741 <solo741@yandex.ru>
2018-10-23 16:38:50 +0400

    Изменила break на exit. Проверку if (!isset($argv[‘1’])) вынесла в начало.

--- visaconsole.php
+++ visaconsole.php
@@ -1,4 +1,10 @@
 <?php
+if (!isset($argv['1'])) {
+    $notice1= 'Введите название страны: все слова пишите с большой буквы';
+    $notice1=iconv('utf-8', 'windows-1251', $notice1);
+    exit($notice1);
+}
+
 ini_set('display_errors', 'Off');
 if ($fp = fopen("https://data.gov.ru/opendata/7704206201-country/data-20180609T0649-structure-20180609T0649.csv?encoding=UTF-8", "r") or $fp = fopen("https://raw.githubusercontent.com/netology-code/php-2-homeworks/master/files/countries/opendata.csv", "r")) {
     while ($data = fgetcsv($fp, 0, ",")):
@@ -6,8 +12,9 @@ if ($fp = fopen("https://data.gov.ru/opendata/7704206201-country/data-20180609T0
     endwhile;
     fclose($fp);
 } else {
-echo 'Сервис временно недоступен';
-    break;
+    $notice2= 'Сервис временно недоступен';
+    $notice2=iconv('utf-8', 'windows-1251', $notice2);
+    exit($notice2);
 }
 foreach ($list as $key => $value) {
     $column1[] = $value[1];
@@ -15,12 +22,6 @@ foreach ($list as $key => $value) {
 }
 $registry = array_combine($column1, $column4);
 
-if (!isset($argv['1'])) {
-    $notice1= 'Введите название страны: все слова пишите с большой буквы';
-    $notice1=iconv('utf-8', 'windows-1251', $notice1);
-    echo $notice1;
-    break;
-}
 $country = array_slice($argv, 1);
 $country=implode(' ', $country);
 
@@ -29,9 +30,9 @@ $country = iconv('windows-1251', 'utf-8', $country);
 
 
 if (isset($country) && array_key_exists($country, $registry) == false) {
-    $notice2= 'Введите правильное название';
-    $notice2=iconv('utf-8', 'windows-1251', $notice2);
-    echo $notice2;
+    $notice3= 'Введите правильное название';
+    $notice3=iconv('utf-8', 'windows-1251', $notice3);
+    echo $notice3;
  
 } else {
     foreach ($registry as $land => $mode) {
